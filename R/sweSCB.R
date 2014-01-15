71128b301771993be096b140c0a19ab5f413c03e
Love Hansson <love.hansson@gmail.com>
2014-01-15 00:00:00 +0000

    version 0.3

---
+++ R/sweSCB.R
@@ -0,0 +1,8 @@
+#' @title sweSCB
+#' 
+#' @description A package for interfacing with the API of Statistics Sweden
+#' 
+#' @name sweSCB
+#' @docType package
+#' @import data.table httr RJSONIO stringr reshape2
+NULL
